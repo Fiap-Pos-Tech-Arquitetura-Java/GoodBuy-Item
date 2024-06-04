cef71002de89c703d7d02a129fc7698ebbff8d43
Anderson0Wagner <anderson.wagner@gmail.com>
2024-06-03 22:11:41 -0300

    criação do projeto com spring boot

---
+++ src/main/java/br/com/fiap/postech/GoodBuy_Item/GoodBuyItemApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.GoodBuy_Item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodBuyItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodBuyItemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/postech/GoodBuy_Item/GoodBuyItemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.GoodBuy_Item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodBuyItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
