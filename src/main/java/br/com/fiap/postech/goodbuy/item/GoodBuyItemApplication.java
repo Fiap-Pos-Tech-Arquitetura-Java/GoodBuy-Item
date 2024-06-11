6bc1b09af6f86c29665caecc29bb92e833ed9d6f
Anderson0Wagner <anderson.wagner@gmail.com>
2024-06-11 13:15:12 -0300

    renomeando diretorio.

--- src/main/java/br/com/fiap/postech/goodbuy/item/GoodBuyItemApplication.java
+++ src/main/java/br/com/fiap/postech/goodbuy/item/GoodBuyItemApplication.java
@@ -1,4 +1,4 @@
-package br.com.fiap.postech.GoodBuy_Item;
+package br.com.fiap.postech.goodbuy.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/fiap/postech/goodbuy/item/GoodBuyItemApplicationTests.java
+++ src/test/java/br/com/fiap/postech/goodbuy/item/GoodBuyItemApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.fiap.postech.GoodBuy_Item;
+package br.com.fiap.postech.goodbuy.item;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
